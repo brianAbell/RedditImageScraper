57f0757e0146f3c5342c054cebba3306d471b0f1
Brian Bell <bbell.semm@gmail.com>
2023-10-23 14:45:47 -0400

    Add download image functionality

--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
 import os
 import praw
+import requests
 
 # Load secrets from .env file
 client_id = os.getenv('REDDIT_CLIENT_ID')
@@ -24,3 +25,35 @@ def get_image_posts(subreddit_name, limit=10):
             posts.append(post)
     
     return posts
+
+def download_images(posts):
+    """
+    Downloads images from a list of Reddit posts.
+
+    Args:
+    - posts (list): A list of Reddit post objects fetched using PRAW.
+
+    Notes:
+    - Only downloads images with 'jpg', 'jpeg', or 'png' extensions.
+    - Uses the post's URL to download the image.
+    - Saves the image with its original name in the current directory.
+    """
+
+    # Loop through each post object in the list
+    for post in posts:
+        
+        # Extract the direct URL to the image from the post
+        image_url = post.url
+        
+        # Extract the image's filename from its URL.
+        # This is done by splitting the URL at each "/" and taking the last segment.
+        image_name = image_url.split("/")[-1]
+        
+        # Open (or create) an image file in binary write mode
+        with open(image_name, 'wb') as f:
+            
+            # Send a GET request to the image URL to fetch the raw image content
+            image = requests.get(image_url).content
+            
+            # Write the raw image content to the file
+            f.write(image)
