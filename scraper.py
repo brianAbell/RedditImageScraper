622b479426380c9a7cd395dff09245b49d3594f1
Brian Bell <bbell.semm@gmail.com>
2023-10-23 14:42:30 -0400

    Fetch posts from subreddit

--- scraper.py
+++ scraper.py
@@ -12,3 +12,15 @@ reddit = praw.Reddit(client_id=client_id,
                      user_agent='reddit_image_scraper',
                      username=username,
                      password=password)
+
+# Fetch posts from subreddit
+def get_image_posts(subreddit_name, limit=10):
+    subreddit = reddit.subreddit(subreddit_name)
+    posts = []
+
+    # for each post in 'hot' category, add valid image to list
+    for post in subreddit.hot(limit=limit):
+        if post.url.endswith(('jpg', 'jpeg', 'png')):
+            posts.append(post)
+    
+    return posts
